0a0057720936fd70f1e0902d89c405c9ab64485c
Yelim <105343281+yelimlikelion@users.noreply.github.com>
2022-08-18 13:12:57 +0900

    resnet
    loss 추출을 위한 pretrained model

---
+++ style_extract/resnet.py
@@ -0,0 +1,23 @@
+
+import torch.nn as nn
+import torchvision.models as models
+
+# import pretrained resnet model
+resnet = models.resnet152(pretrained=True)
+for param in resnet.parameters():
+    param.requires_grad = False
+
+class Resnet(nn.Module):
+    def __init__(self):
+        super(Resnet, self).__init__()
+        self.layer0 = nn.Sequential(*list(resnet.children())[0:1])
+        self.layer1 = nn.Sequential(*list(resnet.children())[1:4])
+        self.layer2 = nn.Sequential(*list(resnet.children())[4:5])
+        self.layer3 = nn.Sequential(*list(resnet.children())[5:6])
+    def forward(self, x):
+        out_0 = self.layer0(x)
+        out_1 = self.layer1(out_0)
+        out_2 = self.layer2(out_1)
+        out_3 = self.layer3(out_2)
+        
+        return out_0, out_1, out_2, out_3
